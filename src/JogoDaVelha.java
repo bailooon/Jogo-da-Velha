b1828bbd26158e678f51eecdc5f9b1065ff15766
bailooon <matheus@bailon.com.br>
2025-04-06 16:16:59 -0300

    adicionando a grid do tabuleiro e sua personalização

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -1,34 +1,60 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
 import javax.swing.*;
 
 public class JogoDaVelha {
     int larguraTabuleiro = 600;
     int alturaTabuleiro = 650;
     
-    JFrame frame = new JFrame("Jogo da Velha");
-    JLabel textLabel = new JLabel();
-    JPanel textPanel = new JPanel();
+    JFrame tela = new JFrame("Jogo da Velha");
+    JLabel textoRotulo = new JLabel();
+    JPanel textoPainel = new JPanel();
+    JPanel tabuleiroPainel = new JPanel();
+
+    JButton[][] tabuleiro = new JButton[3][3];
+    String jogadorX = "X";
+    String jogadorO = "O";
+    String jogadorAtual = jogadorX;
 
     public JogoDaVelha() {
-        frame.setVisible(true);
-        frame.setSize(larguraTabuleiro, alturaTabuleiro);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
+        tela.setVisible(true);
+        tela.setSize(larguraTabuleiro, alturaTabuleiro);
+        tela.setLocationRelativeTo(null);
+        tela.setResizable(false);
+        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tela.setLayout(new BorderLayout());
 
-        textLabel.setBackground(Color.darkGray);
-        textLabel.setForeground(Color.white);
-        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
-        textLabel.setHorizontalAlignment(JLabel.CENTER);
-        textLabel.setText("Jogo da Velha");
-        textLabel.setOpaque(true);
+        textoRotulo.setBackground(Color.darkGray);
+        textoRotulo.setForeground(Color.white);
+        textoRotulo.setFont(new Font("Arial", Font.BOLD, 50));
+        textoRotulo.setHorizontalAlignment(JLabel.CENTER);
+        textoRotulo.setText("Jogo da Velha");
+        textoRotulo.setOpaque(true);
 
-        textPanel.setLayout(new BorderLayout());
-        textPanel.add(textLabel);
-        frame.add(textPanel, BorderLayout.NORTH);
+        textoPainel.setLayout(new BorderLayout());
+        textoPainel.add(textoRotulo);
+        tela.add(textoPainel, BorderLayout.NORTH);
+
+        tabuleiroPainel.setLayout(new GridLayout(3, 3));
+        tabuleiroPainel.setBackground(Color.darkGray);
+        tela.add(tabuleiroPainel);
+
+        for (int v=0; v<3; v++){
+            for (int c=0; c<3; c++){
+                JButton casa = new JButton();
+                tabuleiro[v][c] = casa;
+                tabuleiroPainel.add(casa);
+
+                casa.setBackground(Color.darkGray);
+                casa.setForeground(Color.white);
+                casa.setFont(new Font("Arial", Font.BOLD, 120));
+                casa.setFocusable(false);
+
+                
+            }
+        }
     }
 
     
