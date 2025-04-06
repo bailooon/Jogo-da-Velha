58d2e6f48a92cef9c0e3282a13643b23ef0dfcb4
bailooon <matheus@bailon.com.br>
2025-04-06 15:46:16 -0300

    adicionando o cabeçalho com o nome do jogo

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -1,12 +1,15 @@
 import java.awt.BorderLayout;
-
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.*;
 
 public class JogoDaVelha {
     int larguraTabuleiro = 600;
     int alturaTabuleiro = 650;
     
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("Jogo da Velha");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
 
     public JogoDaVelha() {
         frame.setVisible(true);
@@ -15,6 +18,17 @@ public class JogoDaVelha {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+        textLabel.setBackground(Color.darkGray);
+        textLabel.setForeground(Color.white);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Jogo da Velha");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
     }
 
     
